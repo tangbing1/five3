546c9a21178eb321d2278423796fa7830b9f47eb
asus <1111>
2018-07-16 15:16:58 +0800

    利用session实现状态登录,注销功能

--- apps/cms/index_view.py
+++ apps/cms/index_view.py
@@ -1,25 +1,39 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session, flash
 from apps.cms import cms_bp
 # from apps.models.user import User
 from apps.models.base import db
 from apps.models.user import User
-from apps.seller_forms.seller_forms import SellerResForm
+from apps.seller_forms.seller_forms import SellerResForm, SellerLoginForm
 
 
 @cms_bp.route('/index/',endpoint='主页')
 def index():
+    # print(session['username'])
+    if session:
+
+        return render_template('cms/index.html',name=session)
     return render_template('cms/index.html')
 
+# 登录功能
 @cms_bp.route('/login/',methods=['GET','POST'],endpoint='登录')
 def i_login():
-    name = request.form.get('username')
-    password = request.form.get('password')
-    num = User.query.filter(User.username == name, User._password == password).count()
-    if num:
-        return redirect(url_for('cms.主页'))
+    if request.method == 'POST':
+        user_form = SellerLoginForm(request.form)
+        if user_form.validate():
+           num = User.query.filter(User.username == user_form.username.data,
+                              User._password == user_form.password1.data).count()
+           if num:
+               session.permanent = True
+               session['username'] = request.form['username']
+               flash('You were logged in')
+               # 利用session来记录登录状态
+               return redirect(url_for('cms.主页'))
+        return render_template('cms/login.html', user_form=user_form)
     return render_template('cms/login.html')
 
 
+
+# 注册功能
 @cms_bp.route('/res/', methods=['GET', 'POST'], endpoint='注册')
 def i_res():
     if request.method == 'POST':
@@ -41,3 +55,9 @@ def i_res():
     else:
 
         return render_template('cms/res.html')
+
+@cms_bp.route('/logout/',endpoint='注销')
+def i_logout():
+    session.permanent = False
+    session['username'] = ''
+    return redirect(url_for('cms.主页'))
--- apps/seller_forms/seller_forms.py
+++ apps/seller_forms/seller_forms.py
@@ -24,3 +24,17 @@ class SellerResForm(Form):
     def validate_username(self, field):
         if User.query.filter(User.username == field.data).count() == 1:
             raise ValidationError('该用户名已经存在')
+
+class SellerLoginForm(Form):
+    username = StringField(label='用户名',
+                           validators=[validators.InputRequired(message='用户名必须填写'),
+                                       validators.Length(min=3, message='不能少于3个字符'),
+                                       validators.Length(max=32, message='不能多于32个字符')], )
+    password1 = PasswordField(label='密码',
+                              validators=[validators.InputRequired(message='密码必须填写'),
+                                          validators.Length(min=3, message='不能少于6个字符'),
+                                          validators.Length(max=32, message='不能多于16个字符')], )
+
+    def validate_username(self, field):
+        if User.query.filter(User.username == field.data).count() == 0:
+            raise ValidationError('该用户名还没注册')
